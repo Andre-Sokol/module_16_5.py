26f51da9d0e70b872efc4880952653dcc9e32b60
Andre-Sokol <tehburoandre@mail.ru>
2025-01-28 13:06:08 +0500

    Шаблонизатор Jinja 2
    Задача "Список пользователей в шаблоне":
    Подготовка:
    Используйте код из предыдущей задачи.
    Скачайте заготовленные шаблоны для их дополнения.
    Шаблоны оставьте в папке templates у себя в проекте.
    Создайте объект Jinja2Templates, указав в качестве папки шаблонов - templates.
    Измените и дополните ранее описанные CRUD запросы:
    Напишите новый запрос по маршруту '/':
    Функция по этому запросу должна принимать аргумент request и возвращать TemplateResponse.
    TemplateResponse должен подключать ранее заготовленный шаблон 'users.html', а также передавать в него request и список users. Ключи в словаре для передачи определите самостоятельно в соответствии с шаблоном.
    Измените get запрос по маршруту '/user' на '/user/{user_id}':
    Функция по этому запросу теперь принимает аргумент request и user_id.
    Вместо возврата объекта модели User, теперь возвращается объект TemplateResponse.
    TemplateResponse должен подключать ранее заготовленный шаблон 'users.html', а также передавать в него request и одного из пользователей - user. Ключи в словаре для передачи определите самостоятельно в соответствии с шаблоном.
    Создайте несколько пользователей при помощи post запроса со следующими данными:
    username - UrbanUser, age - 24
    username - UrbanTest, age - 22
    username - Capybara, age - 60
    В шаблоне 'users.html' заготовлены все необходимые теги и обработка условий, вам остаётся только дополнить закомментированные строки вашим Jinja 2 кодом (использование полей id, username и age объектов модели User):
    1. По маршруту '/' должен отображаться шаблон 'users.html' со списком все ранее созданных объектов:

---
+++ module_16_5.py
@@ -0,0 +1,65 @@
+
+from fastapi import FastAPI, Path, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True}, debug=True)
+templates = Jinja2Templates(directory="templates")
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+@app.get('/', response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
+
+@app.get('/user/{user_id}', response_class=HTMLResponse)
+async def get_user_user_id(request: Request, user_id: int = Path(ge=1, le=100,
+                                 description='Enter User ID', example='1')):
+    for i in users:
+        if i.id == user_id:
+            return templates.TemplateResponse(
+                request=request, name='users.html', context={'user': i})
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.post("/user/{username}/{age}")
+async def post_users(
+        username: str = Path(min_length=5, max_length=20, description='Enter Username', example='UrbanUser'),
+        age: int = Path(ge=18, le=120, description='Enter Age', example='24')):
+    user_id = (users[-1].id + 1) if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int, username: str = Path(min_length=5, max_length=20,
+                      description='Enter Username', example='UrbanUser'),
+                      age: int = Path(ge=18, le=120, description='Enter Age', example='24')):
+    try:
+        user = (user for user in users if user.id == user_id)
+        user.username = username
+        user.age = age
+        return user
+    except StopIteration:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: str = Path(min_length=1, max_length=1000, description='Enter User ID')) -> str:
+    try:
+        users.pop(user_id)
+        return f"User id = {user_id} deleted"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User not found")
